8774bb41f577da8a0ad903d9425944257b52ea6f
nikerblack <nikerblack@hotmail.com>
2022-07-18 09:56:39 -0500

    first commit

---
+++ config-client-app/src/test/java/com/sergio/configclientapp/ConfigClientAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.configclientapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigClientAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
